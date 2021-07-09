157cdb5ab679fe90828190a0ad952a40102faed4
peacegypsy <christinasherman206@gmail.com>
2021-07-09 08:31:57 -0700

    wave 01 and 02 passing. Welcome to the strugglebus.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 import Board from './components/Board';
 
-const PLAYER_1 = 'X';
-const PLAYER_2 = 'O';
+const PLAYER_1 = 'x';
+const PLAYER_2 = 'o';
 
 const generateSquares = () => {
   const squares = [];
@@ -30,14 +30,46 @@ const App = () => {
   // This starts state off as a 2D array of JS objects with
   // empty value and unique ids.
   const [squares, setSquares] = useState(generateSquares());
-
+  const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+  //const winner = checkForWinner(squares);
   // Wave 2
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
+  const onClickCallback = (id) => {
+    let row = 0
+    while (id > 2) {
+      id -= 3
+      row++
+    }
+    if (!squares[row][id]['value']){
+      const newSquares = [...squares]
+      newSquares[row][id]['value'] = currentPlayer
+      setSquares(newSquares)
 
+      const updatedPlayer = currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1
+      setCurrentPlayer(updatedPlayer)}
+  }
 
   const checkForWinner = () => {
+    const possibleLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    // go over all possibly winning lines and check if they consist of only X's/only O's
+    for (let i = 0; i < possibleLines.length; i++) {
+      const [a, b, c] = possibleLines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
     // Complete in Wave 3
     // You will need to:
     // 1. Go accross each row to see if 
@@ -49,20 +81,22 @@ const App = () => {
     //    all three squares have the same value.
 
   }
-
+  
   const resetGame = () => {
-    // Complete in Wave 4
+ 
   }
+    // Complete in Wave 4
+  
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
         <h2>The winner is ... -- Fill in for wave 3 </h2>
-        <button>Reset Game</button>
+        <button className='resetButton' onClick={resetGame}>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback={onClickCallback} />
       </main>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,23 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 import './Square.css'
 
-const Square = (props) => {
+const Square = ({id, value, onClickCallback}) => {
+
+
+  const markSquare = () => {
+    onClickCallback(id);
+  }
   // For Wave 1 enable this 
   //  Component to alert a parent 
   //  component when it's clicked on.
 
-  return <button
+  return <button 
+    onClick={markSquare}
     className="square"
   >
-    {props.value}
+    {value}
   </button>
 }
 
