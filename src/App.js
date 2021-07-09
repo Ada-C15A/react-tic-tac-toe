e14366cf47cd6b6efcbc64ce095a958cc5801fca
RETURNcaden <cadence1547@gmail.com>
2021-07-09 08:42:11 -0700

    add squares and create x and o

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Board from './components/Board';
 
 const PLAYER_1 = 'X';
 const PLAYER_2 = 'O';
-
+let currentPlayer=PLAYER_1;
 const generateSquares = () => {
   const squares = [];
 
@@ -21,8 +21,9 @@ const generateSquares = () => {
       currentId += 1;
     }
   }
-
+  console.log('ss',squares)
   return squares;
+  
 }
 
 const App = () => {
@@ -35,6 +36,42 @@ const App = () => {
   // You will need to create a method to change the square 
   //   When it is clicked on.
   //   Then pass it into the squares as a callback
+   const onClickCallback=(returningId)=>{
+   console.log('id',returningId)
+
+   const newSquares = []
+   let currentId=0
+ 
+
+      for (let row =0; row<squares.length;row+=1){
+        newSquares.push([])
+        for(let col=0;col<squares.length; col+=1){
+          console.log('row col',squares[row][col].id)
+          if (squares[row][col].id === returningId){
+            newSquares[row].push({
+              id:currentId,
+              value:currentPlayer
+            })
+            currentId+=1
+          }else{
+            newSquares[row].push(squares[row][col])
+          }
+         
+        }
+      }
+      console.log(' square',squares)
+     console.log('nnew square',newSquares)
+   
+      setSquares(newSquares);
+
+      if (currentPlayer === PLAYER_1) {
+        currentPlayer = PLAYER_2;
+      } else if (currentPlayer === PLAYER_2) {
+        currentPlayer =PLAYER_1;
+      }
+  }
+
+  
 
 
   const checkForWinner = () => {
@@ -62,7 +99,7 @@ const App = () => {
         <button>Reset Game</button>
       </header>
       <main>
-        <Board squares={squares} />
+        <Board squares={squares} onClickCallback={onClickCallback}/>
       </main>
     </div>
   );
