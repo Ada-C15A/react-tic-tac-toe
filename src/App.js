9d3c8e99048be587fb6ad4589198a555d876d24f
denisseai <msdenisseanaya@gmail.com>
2021-07-08 19:18:26 -0700

    added emoji

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>{winner === null ? `` : `Winner is ${ winner } !!! `}</h2>
+        <h2>{winner === null ? `` : `🎉 Winner is ${ winner } !!! `}</h2>
         <button className='resetButton' onClick={resetGame}>Reset Game</button>
       </header>
       <main>
