1d6e5e381d96f514a15fc1ee1d70f3e616410dbf
Cloudy Lopez <CloudyLopez@icloud.com>
2021-07-09 10:03:45 -0700

    wave 4 complete - passing all tests. wave 3 broken and im working on it

--- src/App.js
+++ src/App.js
@@ -29,19 +29,49 @@ const generateSquares = () => {
 const App = () => {
   const [squares, setSquares] = useState(generateSquares());
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+  const [winner, setWinner] = useState('')
 
+  const onClickCallback = (id) => {
+    let row = 0;
+    while (id > 2) {
+      id -= 3
+      row ++
+    }
+  }
 
-  const checkForWinner = () => {
+  const checkForWinner = (row, column) => {
     // Complete in Wave 3
     // You will need to:
     // 1. Go accross each row to see if 
     //    3 squares in the same row match
     //    i.e. same value
+    if (squares[row][0]['value'] === currentPlayer &&
+      squares[row][1]['value'] === currentPlayer &&
+      squares[row][2]['value'] === currentPlayer) {
+      setWinner(currentPlayer)
+      return true
+    }
     // 2. Go down each column to see if
     //    3 squares in each column match
+    if (squares[0][column]['value'] === currentPlayer &&
+      squares[1][column]['value'] === currentPlayer &&
+      squares[2][column]['value'] === currentPlayer) {
+      setWinner(currentPlayer)
+      return true
+    }
     // 3. Go across each diagonal to see if 
     //    all three squares have the same value.
-
+    if ((squares[0][0]['value'] === currentPlayer &&
+      squares[1][1]['value'] === currentPlayer &&
+      squares[2][2]['value'] === currentPlayer) || 
+      (squares[0][2]['value'] === currentPlayer &&
+      squares[1][1]['value'] === currentPlayer &&
+      squares[2][0]['value'] === currentPlayer)
+    ) {
+      setWinner(currentPlayer)
+      return true
+    }
+    return false
   }
 
   const updateSquares = (id) => {
@@ -72,15 +102,17 @@ const App = () => {
   }
 
   const resetGame = () => {
-    // Complete in Wave 4
+    setSquares(generateSquares());
+    setCurrentPlayer(PLAYER_1);
+    setWinner('');
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
-        <button>Reset Game</button>
+        <h2>{winner == null ? `` : `The winner is ${winner}`} </h2>
+        <button onClick={resetGame}>Reset Game</button>
       </header>
       <main>
         <Board squares={squares} onClickCallback={updateSquares}/>
